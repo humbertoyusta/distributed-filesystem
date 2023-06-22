fbf302693cc4058af55ebdfb20d2a63a05eccc48
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-06-22 10:51:51 +0200

    fix/chunkserver

--- chunkserver/app.py
+++ chunkserver/app.py
@@ -8,39 +8,43 @@ app.config['UPLOAD_FOLDER'] = '/chunks'
 
 
 @app.route('/store/<chunkname>', methods=['POST'])
-def store_chunk(chunkname):
+def store_chunk(chunkname: str):
     if 'file' not in request.files:
         return 'No file part', 400
 
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
     file = request.files['file']
-    filename = chunkname
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], chunkname))
 
     return 'File stored successfully', 200
 
 
 @app.route('/retrieve/<chunkname>', methods=['GET'])
-def retrieve_chunk(chunkname):
-    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], chunkname))
+def retrieve_chunk(chunkname: str):
+    try:
+        return send_file(os.path.join(app.config['UPLOAD_FOLDER'], chunkname))
+    except FileNotFoundError:
+        return 'File not found', 404
 
 
 @app.route('/delete/<chunkname>', methods=['DELETE'])
-def delete_chunk(chunkname):
+def delete_chunk(chunkname: str):
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], chunkname)
     try:
         os.remove(file_path)
         return 'File deleted successfully', 200
-    except:
+    except FileNotFoundError:
         return 'File not found', 404
 
 
 @app.route('/size/<chunkname>', methods=['GET'])
-def size_chunk(chunkname):
+def size_chunk(chunkname: str):
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], chunkname)
     try:
         size = os.path.getsize(file_path)
         return str(size), 200
-    except:
+    except FileNotFoundError:
         return 'File not found', 404
 
 
