b3b289fa32edb2b64bc3fee25f23e9d240656bbd
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-06-23 11:25:07 +0200

    feature/add-chunk-server

--- chunk_server/app.py
+++ chunk_server/app.py
@@ -48,5 +48,10 @@ def size_chunk(chunkname: str):
         return 'File not found', 404
 
 
+@app.route('/health', methods=['GET'])
+def health():
+    return 'OK', 200
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
